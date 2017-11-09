eb52ef4dac15c45ae9885704165ca2a7e51ed1a8
Danny Angelo Carminati Grein <fungos@gmail.com>
2017-11-08 19:02:59 -0500

    Update fips-assimp to 9a13bf2

--- revision.h
+++ revision.h
@@ -1,7 +1,7 @@
 #ifndef ASSIMP_REVISION_H_INC
 #define ASSIMP_REVISION_H_INC
 
-#define GitVersion 0
+#define GitVersion 0x9a13bf2
 #define GitBranch "master@fips-assimp"
 
 #endif // ASSIMP_REVISION_H_INC
